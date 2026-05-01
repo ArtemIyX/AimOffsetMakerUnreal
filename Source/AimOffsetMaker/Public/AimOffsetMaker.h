330c01933c7534079d32fe810e3e32c9b2816a96
Artem Podorozhko <68477399+ArtemIyX@users.noreply.github.com>
2026-05-01 14:24:07 +0300

    Add new methods and properties to AimOffsetMaker module

--- Source/AimOffsetMaker/Public/AimOffsetMaker.h
+++ Source/AimOffsetMaker/Public/AimOffsetMaker.h
@@ -1,27 +1,37 @@
 #pragma once
 
+#include "Containers/Array.h"
 #include "Templates/SharedPointer.h"
 #include "Modules/ModuleManager.h"
 
 class FSlateStyleSet;
+class SWindow;
 class UAnimSequence;
 
 class FAimOffsetMakerModule : public IModuleInterface
 {
 public:
+#pragma region ModuleOverrides
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+#pragma endregion
 
+#pragma region Methods
 private:
 	void RegisterMenus();
 	void RegisterStyle();
 	void UnregisterStyle();
 	void OpenAimOffsetDialog() const;
+	void OnAimOffsetDialogClosed(const TSharedRef<SWindow>& InWindow) const;
 	UAnimSequence* GetCurrentAnimSequenceFromContext(const struct FToolMenuContext& InContext) const;
 	bool IsAnimSequenceEditorContext(const struct FToolMenuContext& InContext) const;
+#pragma endregion
 
+#pragma region Properties
 private:
 	FString ButtonIconName = TEXT("ButtonIcon16");
 	mutable TWeakObjectPtr<UAnimSequence> PendingDialogSequence;
+	mutable TArray<TWeakPtr<SWindow>> OpenDialogWindows;
 	TSharedPtr<FSlateStyleSet> StyleSet;
+#pragma endregion
 };
